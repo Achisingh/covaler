e468b465af604941a950ee5b856b2fa6acd0e171
Achisingh <achala.singh94@gmail.com>
2023-07-24 14:28:08 +1000

    code cleanup

--- src/components/productsList/productsList.component.jsx
+++ src/components/productsList/productsList.component.jsx
@@ -7,10 +7,12 @@ const ProductList = ({ products }) => {
   const [total, setTotal] = useState(0);
   const [discountedPrice, setDiscountedPrice] = useState(0);
 
+  // Add to Cart on button Click for products
   const addToCart = (product) => {
     setCart([...cart, product]);
   };
 
+  // Calculate cart total without discount
   const calculateCartTotal = () => {
     let total = 0;
     for (let product of cart) {
@@ -19,6 +21,7 @@ const ProductList = ({ products }) => {
     return total;
   };
 
+  // Discount applied
   const calculateDiscountedPrice = () => {
     const cartTotal = calculateCartTotal();
     let price = cartTotal;
@@ -36,6 +39,7 @@ const ProductList = ({ products }) => {
     return price;
   };
 
+  // Update states on useEffects
   useEffect(() => {
     const price = calculateDiscountedPrice();
     setTotal(calculateCartTotal());
@@ -44,6 +48,7 @@ const ProductList = ({ products }) => {
 
   return (
     <div>
+      {/* Show products */}
       <div className="product-list-container">
         <h2>Available Products</h2>
         <div className="product-boxes">
@@ -71,6 +76,7 @@ const ProductList = ({ products }) => {
           ))}
         </div>
       </div>
+      {/* Checkbox for member products */}
       <div>
         <label>
           <input
@@ -81,12 +87,14 @@ const ProductList = ({ products }) => {
           Member
         </label>
       </div>
+      {/* Show Both totals */}
       <div className="cart-total">
         <h2>Cart Total</h2>
         <p>Total : {total.toFixed(2)}</p>
         <h2>Discount Total</h2>
         <p>Total : {discountedPrice.toFixed(2)}</p>
       </div>
+      {/* Show Cart products */}
       <div className="product-list-container">
         <h2>Cart Items</h2>
         <div className="product-boxes">
